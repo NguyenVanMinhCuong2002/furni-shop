3d057ab4ac42ad57c87d1ec72c6ed7e64472da21
nvmcuong2002 <nvmcuong2002@gmail.com>
2025-09-18 10:36:41 +0700

    completet02-build

--- dist/routes/about.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const aboutRouter = (0, express_1.Router)();
-const path = "/about/";
-aboutRouter.get("/about", (req, res) => {
-    return res.render("pages/about", { title: "Trang chủ", message: "Xin chào MVC với TypeScript!" });
-});
-exports.default = aboutRouter;
