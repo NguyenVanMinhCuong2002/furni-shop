2a3ff8822c0f5aa11464a93167959975f590c963
nvmcuong2002 <nvmcuong2002@gmail.com>
2025-09-22 22:58:01 +0700

    fix bug

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -7,7 +7,7 @@ const host = process.env.DATABASE_HOST
 const port = process.env.DATABASE_PORT
 const user = process.env.DATABASE_USER
 const password = process.env.DATABASE_PASSWORD
-const db_name = process.env.DATABASE_name
+const db_name = process.env.DATABASE_NAME
 
 const pgp = pgPromise();
 const db = pgp(`postgres://${user}:${password}@${host}:${port}/${db_name}`);
