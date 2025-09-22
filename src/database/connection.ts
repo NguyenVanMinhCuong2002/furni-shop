dd08d73c26126c62e6a384754390b738758cfff2
nvmcuong2002 <nvmcuong2002@gmail.com>
2025-09-22 17:49:20 +0700

    fix bug

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,6 +1,15 @@
 import pgPromise from 'pg-promise';
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const host = process.env.DATABASE_HOST
+const port = process.env.DATABASE_PORT
+const user = process.env.DATABASE_USER
+const password = process.env.DATABASE_PASSWORD
+const db_name = process.env.DATABASE_name
 
 const pgp = pgPromise();
-const db = pgp('postgres://postgres:postgres@localhost:5432/chairshop');
+const db = pgp(`postgres://${user}:${password}@${host}:${port}/${db_name}`);
 
 export default db;
